ee0070a0c9facbd909424fc013d514a67038c17f
Ransilu Samaraweera <yasitha5718@gmail.com>
2025-08-29 09:43:59 +0530

    commit 1

--- backend/pos-system-admin/src/app.js
+++ backend/pos-system-admin/src/app.js
@@ -7,12 +7,14 @@ mongoose.connect(MONGO_URI)
 import express from 'express';
 import authRoutes from './routes/auth.js';
 import dashboardRoutes from './routes/dashboard.js';
+import productsRoutes from './routes/products.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/dashboard', dashboardRoutes);
+app.use('/api/products', productsRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
