a44c58a34dc9a71034086dd362f59bd302eaed32
Pavan Uthsara <uthsarapavan@gmail.com>
2025-08-31 15:26:49 +0530

    fix the bug in dashboard.js

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getDashboard } from '../controllers/dashboardController.js';
-import {} from '../controllers/adminProductController.js';
+import { adminAddProdcut } from '../controllers/adminProductController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 const router = express.Router();
 
