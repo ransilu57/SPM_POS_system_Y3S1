bf95df6f5d7162828fa283a56fd0cd8c8a2e97e3
Damith Chandrathilake <161125812+damithch@users.noreply.github.com>
2025-09-02 18:37:50 +0530

    added middleware for admin/ changed authmidleware file

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use(cors({
 	origin: 'http://localhost:5173',
 	credentials: true
 }));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,12 +1,25 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 
 export default function (req, res, next) {
     const authHeader = req.headers['authorization'];
     if (!authHeader) return res.status(401).json({ message: 'No token provided' });
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, 'your_jwt_secret', (err, user) => {
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return res.status(403).json({ message: 'Invalid token' });
         req.user = user;
         next();
     });
+    
+}
+
+export function adminMiddleware(req, res, next) {
+    if (req.user.role !== 'admin') {
+        return res.status(403).json({ message: 'Access denied: Admins only' });
+    }
+    next();
 }
